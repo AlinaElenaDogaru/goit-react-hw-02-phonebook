820bbf7b6d87312258be39c9f6fa563aef32843c
Alina Dogaru <alina.elena_dogaru@yahoo.com>
2024-10-31 18:37:20 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
+import React, { useState } from 'react';
+import ContactForm from 'components/ContactForm';
+import ContactList from 'components/ContactList';
+import Filter from './Filter';
+import styles from './ContactForm.module.css';
+
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const addContact = (newContact) => {
+    const duplicate = contacts.find(contact => contact.name === newContact.name);
+    if (duplicate) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+    setContacts([...contacts, newContact]);
+  };
+
+  const deleteContact = (id) => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1 className={styles.title}>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2 className={styles.title}>Contacts</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ContactList contacts={filteredContacts} deleteContact={deleteContact} />
     </div>
   );
-};
+}
+
+
+
+export default App;
+
